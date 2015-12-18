5427d818ae9121482559a58b0cc919fbf49b69db
Gus42 <gus815@gmail.com>
2015-12-18 17:02:02 +0000

    From restaurants to catalog
    - database_setup now is for catalog
    - finalProject now has the login methods (google and facebook)

--- database_setup.py
+++ database_setup.py
@@ -1,5 +1,3 @@
-import os
-import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -8,11 +6,22 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
-class Restaurant(Base):
-    __tablename__ = 'restaurant'
+class User(Base):
+    __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -23,16 +32,16 @@ class Restaurant(Base):
         }
 
 
-class MenuItem(Base):
-    __tablename__ = 'menu_item'
+class Item(Base):
+    __tablename__ = 'item'
 
     name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
     description = Column(String(250))
-    price = Column(String(8))
-    course = Column(String(250))
-    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
-    restaurant = relationship(Restaurant)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -40,13 +49,10 @@ class MenuItem(Base):
         return {
             'name': self.name,
             'description': self.description,
-            'id': self.id,
-            'price': self.price,
-            'course': self.course,
+            'id': self.id
         }
 
 
-engine = create_engine('sqlite:///restaurantmenu.db')
-
+engine = create_engine('sqlite:///catalog.db')
 
 Base.metadata.create_all(engine)
